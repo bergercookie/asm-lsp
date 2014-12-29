739f7f5d0f06a9c264a74537ed9d6d39cd059ec7
Marat Dukhan <maratek@gmail.com>
2014-12-29 01:43:07 -0500

    Update setup files

--- setup.py
+++ setup.py
@@ -7,14 +7,20 @@ import os
 
 root_path = os.path.dirname(__file__)
 
+def read_text_file(path):
+    with open(os.path.join(root_path, path)) as file:
+        return file.read()
+
+
 setup(
     name="opcodes",
     version=__version__,
     description="Database of Processor Instructions/Opcodes",
+    long_description=read_text_file("readme.rst"),
     author=__author__,
     author_email=__email__,
     url="https://github.com/Maratyszcza/Opcodes",
-    packages=['opcodes'],
+    packages=["opcodes"],
     keywords=["assembly", "assembler", "asm"],
     requires=[],
     classifiers=[
