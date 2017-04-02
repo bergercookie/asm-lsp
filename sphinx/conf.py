a0d019d0b4abbdba064103279bd6f8ee3502829b
Marat Dukhan <maratek@gmail.com>
2017-04-02 17:47:28 -0400

    Update copyright year

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -11,7 +11,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 project = u'CPU Opcodes'
-copyright = u'2014-2016, Georgia Institute of Technology'
+copyright = u'2014-2017, Georgia Institute of Technology'
 
 version = __version__
 release = __version__
