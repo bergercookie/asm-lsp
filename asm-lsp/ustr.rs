8bda911bddd8e9c5ae0f71f1694d451c2ad28906
WillLillis <wlillis@umass.edu>
2024-11-30 12:59:43 -0500

    chore: add safety doc comments to ustr functions

--- asm-lsp/ustr.rs
+++ asm-lsp/ustr.rs
@@ -2,11 +2,23 @@
 //! input unnecessarily and from my survey our inputs are mostly ascii
 use std::str;
 
+/// Converts a byte slice to a string slice without checking for valid UTF-8.
+///
+/// # Safety
+///
+/// The caller must ensure that the input slice is valid UTF-8. Passing invalid UTF-8
+/// to this function invokes undefined behavior.
 #[must_use]
 pub const fn get_str(v: &[u8]) -> &str {
     unsafe { str::from_utf8_unchecked(v) }
 }
 
+/// Converts a byte vector to a string without checking for valid UTF-8.
+///
+/// # Safety
+///
+/// The caller must ensure that the input vector is valid UTF-8. Passing invalid UTF-8
+/// to this function invokes undefined behavior.
 #[must_use]
 pub fn get_string(v: Vec<u8>) -> String {
     unsafe { String::from_utf8_unchecked(v) }
